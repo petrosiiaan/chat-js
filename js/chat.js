9714beb3510e0bdaf308249494c67467d26e821a
jsonPet <vahe.petrosyan2.y@tumo.org>
2021-01-14 18:55:23 +0400

    last last

--- js/chat.js
+++ js/chat.js
@@ -4,8 +4,8 @@ const LOGGED_IN_USER = URL + routes.getUsers()
 request("GET", LOGGED_IN_USER)
     .then((data) => {
         profId.textContent = data.message.id 
-        userName.textContent = data.message.username
-        email.textContent = data.message.email 
+        userName.textContent += data.message.username
+        email.textContent += data.message.email 
         usName.textContent += data.message.username
 })
 
